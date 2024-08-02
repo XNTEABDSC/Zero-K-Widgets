a9a1f7e683d9e0c765f9498fddc600971e41729c
XNTEABDSC <2364400855@qq.com>
2024-08-03 03:19:13 +0800

    faster

--- Widgets/gui_draw_arti_pos.lua
+++ Widgets/gui_draw_arti_pos.lua
@@ -9,6 +9,114 @@ function widget:GetInfo()
 		enabled   = false,
 	}
 end
+
+---@class WpnInfoAndProjs
+---@field WDId WeaponDefId
+---@field aoe number
+---@field lineWidth number
+---@field Projs list<ProjectileId>
+
+---@type list<WpnInfoAndProjs>
+local WatchWpnAndProjs={}
+
+local WatchWpnNames={
+	"bomberheavy_arm_pidr",
+	"chicken_blimpy_dodobomb",
+	"cloakarty_hammer_weapon",
+	"empmissile_emp_weapon",
+	"jumparty_napalm_sprayer",
+	"missileslow_weapon",
+	"napalmmissile_weapon",
+	"nuclear_missile",
+	"raveparty_blue_shocker",
+	"raveparty_green_stamper",
+	"raveparty_orange_roaster",
+	"raveparty_red_killer",
+	"raveparty_violet_slugger",
+	"raveparty_yellow_slammer",
+	"seismic_seismic_weapon",
+	"shiparty_plasma",
+	"shipcarrier_disarm_rocket",
+	"shipheavyarty_plasma",
+	"spidercrabe_arm_crabe_gauss",
+	"staticarty_plasma",
+	"staticheavyarty_plasma",
+	"staticnuke_crblmssl",
+	"tacnuke_weapon",
+	"tankheavyarty_plasma",
+	"tankarty_core_artillery",
+	"veharty_mine",
+	"vehheavyarty_cortruck_rocket"
+}
+
+local wgGetProjectiles=WG.WackyBag.utils.get_proj.GetProjList
+
+local spGetProjectilePosition=Spring.GetProjectilePosition
+local spGetProjectileVelocity=Spring.GetProjectileVelocity
+local spGetProjectileGravity=Spring.GetProjectileGravity
+local spGetGroundHeight=Spring.GetGroundHeight
+
+
+local WDIdToWatchWpn={}
+
+
+local function GetWatchWeaponDefs()
+	local function SetWatchWeaponDef(wd)
+		local innerId=#WatchWpnAndProjs+1
+		WatchWpnAndProjs[innerId]={
+			WDId=wd.id,
+			aoe=wd.damageAreaOfEffect,
+			lineWidth=math.log(wd.damages[0]+1,2),
+			Projs={}
+		}
+		WDIdToWatchWpn[wd.id]=innerId
+	end
+	for key, value in pairs(WatchWpnNames) do
+		SetWatchWeaponDef(WeaponDefNames[value])
+	end
+end
+
+
+local spGetProjectileDefID=Spring.GetProjectileDefID
+
+local function CheckProjs()
+	for _, WDInfo in pairs(WatchWpnAndProjs) do
+		local ProjList=WDInfo.Projs
+		local ProjIndex=1
+		local ProjCount=#ProjList
+		while ProjIndex<ProjCount do
+			local ProjId=ProjList[ProjIndex]
+			if spGetProjectilePosition(ProjId)==nil	then
+				ProjList[ProjIndex]=ProjList[ProjCount]
+				ProjList[ProjCount]=nil
+				ProjCount=ProjCount-1
+			else
+				ProjIndex=ProjIndex+1
+			end
+		end
+		if ProjIndex==ProjCount then
+			local ProjId=ProjList[ProjIndex]
+			if spGetProjectilePosition(ProjId)==nil	then
+				ProjList[ProjIndex]=nil
+			end
+		end
+	end
+end
+
+local function UpdateProjs()
+	for _, WDInfo in pairs(WatchWpnAndProjs) do
+		WDInfo.Projs={}
+	end
+	WatchProjs={}
+	for key, value in pairs(wgGetProjectiles()) do
+		local WDInnerId=WDIdToWatchWpn[spGetProjectileDefID(value)]
+		if WDInnerId~=nil then
+			local l=WatchWpnAndProjs[WDInnerId].Projs
+			l[#l+1] = value
+		end
+	end
+end
+
 --[[
 local spGetVisibleProjectiles     = SpringRestricted.GetVisibleProjectiles
 ---@type list<projId>
@@ -90,13 +198,10 @@ local glScale                = gl.Scale
 local glTranslate            = gl.Translate
 local glVertex               = gl.Vertex
 local GL_LINES               = GL.LINES
+local GL_LINE_STRIP           = GL.LINE_STRIP
 local GL_LINE_LOOP           = GL.LINE_LOOP
 local GL_POINTS              = GL.POINTS
 
-local spGetProjectilePosition=Spring.GetProjectilePosition
-local spGetProjectileVelocity=Spring.GetProjectileVelocity
-local spGetProjectileGravity=Spring.GetProjectileGravity
-local spGetGroundHeight=Spring.GetGroundHeight
 
 
 local function UnitCircleVertices()
@@ -143,46 +248,7 @@ local function DrawAoe(tx,ty,tz,aoe,alpha2)
 	glColor(1,1,1,1)
 	glLineWidth(1)
 end
----@type {[WeaponDefId]:{aoe:number}}
-local NeededWpnInfo={}
 
-local function GetWatchWeaponDefs()
-	
-	--[[
-	Spring.Echo("TEST")
-	for id,weaponDef in pairs(WeaponDefs) do
-		for name,param in weaponDef:pairs() do
-		  Spring.Echo(name,param)
-		end
-	end
-	]]
-	Spring.Echo("gameSpeed: "..Game.gameSpeed)
-	for id, _ in pairs(WeaponDefs) do
-		local value=WeaponDefs[id]
-		Spring.Echo("Wpn: " .. id .. ", name: " .. value.name)
-		
-		--Spring.Echo("game_message: check wpn: " .. value.name .. ", dmg: " .. tostring( value.damages) .. ", range: " .. tostring( value.range) ..", vel: " .. tostring(value.startvelocity))
-		Spring.Echo("Damages:")
-		PrintTable(value.damages)
-		Spring.Echo("flightTime: " .. tostring(value.flightTime) .. ", range: " .. tostring(value.range).. ", projSpeed: " .. tostring(value.projectilespeed))
-		Spring.Echo("beamTTL:",value.beamTTL,"beamtime: ",value.beamtime)
-		if value.damages and (value.damages[0]>0) and 
-		(not value.flightTime or value.flightTime<=0.1 or value.flightTime>6)  and (not value.beamburst) and
-		--( value.range and 400<value.range and value.projectilespeed and 0<value.projectilespeed and value.projectilespeed*Game.gameSpeed<500 and 0.5<value.range/(value.projectilespeed*Game.gameSpeed) )
-		(value.range and 600<=value.range and value.projectilespeed and 0<value.projectilespeed and value.projectilespeed<50 )and 
-		(value.beamTTL ==0 and value.beamtime==1)
-		then -- and  
-			
-			Spring.Echo("Get Weapon: "  .. value.name)
-			
-			-- Script.SetWatchWeapon(value.id,true)
-			NeededWpnInfo[value.id]={
-				aoe=value.damageAreaOfEffect,
-				lineWidth=math.log(value.damages[0]+1,4)
-			}
-		end
-	end
-end
 
 ---@type {[ProjectileId]:{proOwnerID:UnitId,weaponDefID:WeaponDefId}}
 
@@ -194,6 +260,14 @@ local function EnumProjPath(proID,wpndef)
 	local initTimeCount=Game.gameSpeed*8
 	local timeCount=initTimeCount
 	local posx,posy,posz=spGetProjectilePosition(proID)
+	--[[
+	if posx==nil then
+		Spring.Echo("Odd proj. id: " .. proID .. ", def: " .. WeaponDefs[wpndef].name)
+		--Spring.SetNoPause(false)
+		return function ()
+			return nil
+		end
+	end]]
 	local velx,vely,velz=spGetProjectileVelocity(proID)
 	local grav= spGetProjectileGravity( proID )
 	if 1<WeaponDefs[wpndef].flightTime then
@@ -201,6 +275,44 @@ local function EnumProjPath(proID,wpndef)
 	end
 
 	return function ()
+		timeCount=timeCount-1
+		if 0>=timeCount then
+			return nil
+		end
+		local GroundHeight=spGetGroundHeight(posx,posz)
+		local heightDef=GroundHeight-posy
+		if heightDef>0 then
+			local vlen=heightDef/vely
+			posx,posy,posz=posx-velx*vlen,posy-vely*vlen,posz-velz*vlen
+			local tl=(timeCount-vlen)/initTimeCount
+			timeCount=-1
+			return posx,posy,posz,tl
+		end
+		local oldposx,oldposy,oldposz=posx,posy,posz
+		vely=vely+grav
+		posx,posy,posz=posx+velx,posy+vely,posz+velz
+		return oldposx,oldposy,oldposz,timeCount/initTimeCount
+	end
+end
+--[====[
+---@param proID ProjectileId
+---@param wpndef WeaponDefId
+---@param fn fun(x:number,y:number,z:number,tl:number)
+---@return number
+---@return number
+---@return number
+---@return number
+local function EnumProjPathF(proID,wpndef,fn)
+	local initTimeCount=Game.gameSpeed*8
+	local timeCount=initTimeCount
+	local posx,posy,posz=spGetProjectilePosition(proID)
+	local velx,vely,velz=spGetProjectileVelocity(proID)
+	local grav= spGetProjectileGravity( proID )
+	if 1<WeaponDefs[wpndef].flightTime then
+		grav=0
+	end
+
+	while 0>=timeCount and spGetGroundHeight(posx,posz)>posy do
 		timeCount=timeCount-1
 		if 0>=timeCount then
 			return nil
@@ -214,7 +326,7 @@ local function EnumProjPath(proID,wpndef)
 		return oldposx,oldposy,oldposz,timeCount/initTimeCount
 	end
 end
-
+]====]
 
 ---comment
 ---@param proID ProjectileId
@@ -223,6 +335,14 @@ local function EnumProjPrevPath(proID,wpndef)
 	local initTimeCount=Game.gameSpeed*8
 	local timeCount=initTimeCount
 	local posx,posy,posz=spGetProjectilePosition(proID)
+	--[[
+	if posx==nil then
+		Spring.Echo("Odd proj. id: " .. proID .. ", def: " .. WeaponDefs[wpndef].name)
+		--Spring.SetNoPause(false)
+		return function ()
+			return nil
+		end
+	end]]
 	local velx,vely,velz=spGetProjectileVelocity(proID)
 	velx,vely,velz=-velx,-vely,-velz
 	local grav= spGetProjectileGravity( proID )
@@ -241,29 +361,101 @@ local function EnumProjPrevPath(proID,wpndef)
 			return nil
 		end
 		local oldposx,oldposy,oldposz=posx,posy,posz
-		vely=vely+grav
 		posx,posy,posz=posx+velx,posy+vely,posz+velz
+		vely=vely+grav
 		return oldposx,oldposy,oldposz,timeCount/initTimeCount
 	end
 end
 
-local spGetProjectileDefID=Spring.GetProjectileDefID
-local spGetProjectilesInRectangle=Spring.GetProjectilesInRectangle--Spring.GetVisibleProjectiles
-local wgGetProjectiles=WG.WackyBag.utils.get_proj.GetProjList
 function widget:DrawWorld()
+	CheckProjs()
+	--CheckProjs()
 	mouseDistance= GetMouseDistance() or 1000
-	for _, projid in pairs(wgGetProjectiles()) do
+	local AOEDraws={}
+
+	for _, WDInfo in pairs(WatchWpnAndProjs) do
+		local aoe=WDInfo.aoe
+		local lineWidth=WDInfo.lineWidth
+		local WDId=WDInfo.WDId
+		
+        glLineWidth(lineWidth)
+		local landPos={}
+		for i, projid in pairs(WDInfo.Projs) do
+
+			function DrawPathLine()
+				
+				local enumf=EnumProjPath(projid,WDId)
+
+				
+				local x,y,z,tl
+				while true do
+					local x_,y_,z_,tl_=enumf()
+					if(x_==nil) then
+						break;
+					end
+					x,y,z,tl=x_,y_,z_,tl_
+					glColor(aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl )
+					glVertex(x,y,z)
+				end
+				if tl and 0.1<tl then
+					AOEDraws[#AOEDraws+1] = {x,y,z,aoe,tl}
+				end
+				
+				
+				
+
+
+
+				--[[
+				for x,y,z,tl in enumf() do
+					glColor(aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl )
+					glVertex(x,y,z)
+				end
+				]]
+				--[[
+				local oldx,oldy,oldz,oldtl=enumf()
+				for x,y,z,tl in enumf do
+					glColor(aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* oldtl )
+					glVertex(oldx,oldy,oldz)
+					--EZDrawer.DrawerTemplates.DrawLine(oldx,oldy,oldz,x,y,z,{},WatchWpnInfo[WDId].lineWidth*scatterLineWidthMult/mouseDistance)
+					oldx,oldy,oldz,oldtl=x,y,z,tl
+				end
+				glVertex(oldx,oldy,oldz)
+				]]
+			end
+			glBeginEnd(GL_LINE_STRIP,DrawPathLine)
+			
+			function DrawPrevPathLine()
+				local enumf=EnumProjPrevPath(projid,WDId)
+				local x,y,z,tl=enumf()
+				while x~=nil do
+					glColor(aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl )
+					glVertex(x,y,z)
+					x,y,z,tl=enumf()
+				end
+			end
+			glBeginEnd(GL_LINE_STRIP,DrawPrevPathLine)
+		end
+	end
+
+	for i = 1, #AOEDraws do
+		local t=AOEDraws[i]
+		DrawAoe(t[1],t[2],t[3],t[4],t[5])
+	end
+
+	--[==[
+	for _, projid in pairs(WatchProjs) do
 		local WDId=spGetProjectileDefID(projid)
-		if NeededWpnInfo[WDId] then
+		if WatchWpnInfo[WDId] then
 			do
 				local enumf=EnumProjPath(projid,WDId)
 				local oldx,oldy,oldz,oldtl=enumf()
 				for x,y,z,tl in enumf do
-					EZDrawer.DrawerTemplates.DrawLine(oldx,oldy,oldz,x,y,z,{aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl },NeededWpnInfo[WDId].lineWidth*scatterLineWidthMult/mouseDistance)
+					EZDrawer.DrawerTemplates.DrawLine(oldx,oldy,oldz,x,y,z,{aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl },WatchWpnInfo[WDId].lineWidth*scatterLineWidthMult/mouseDistance)
 					oldx,oldy,oldz,oldtl=x,y,z,tl
 				end
-				if oldtl and 0.1<oldtl and NeededWpnInfo[WDId] then -- hit ground
-					DrawAoe(oldx,oldy,oldz,NeededWpnInfo[WDId].aoe,oldtl)
+				if oldtl and 0.1<oldtl and WatchWpnInfo[WDId] then -- hit ground
+					DrawAoe(oldx,oldy,oldz,WatchWpnInfo[WDId].aoe,oldtl)
 				end
 			end
 			
@@ -271,7 +463,7 @@ function widget:DrawWorld()
 				local enumPrev=EnumProjPrevPath(projid,WDId)
 				local oldx,oldy,oldz,oldtl=enumPrev()
 				for x,y,z,tl in enumPrev do
-					EZDrawer.DrawerTemplates.DrawLine(oldx,oldy,oldz,x,y,z,{aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl },NeededWpnInfo[WDId].lineWidth*scatterLineWidthMult/mouseDistance)
+					EZDrawer.DrawerTemplates.DrawLine(oldx,oldy,oldz,x,y,z,{aoeColor[1],aoeColor[2],aoeColor[3],aoeColor[4]* tl },WatchWpnInfo[WDId].lineWidth*scatterLineWidthMult/mouseDistance)
 					oldx,oldy,oldz,oldtl=x,y,z,tl
 				end
 			end
@@ -281,6 +473,7 @@ function widget:DrawWorld()
 		end
 			
 	end
+	]==]
 end
 
 function widget:Initialize()
@@ -298,6 +491,10 @@ function widget:Shutdown()
 end
 
 local setted=false
-function widget:GameFrame()
-	
+function widget:GameFrame(n)
+	if n%5==0 then
+		UpdateProjs()
+	else
+		--CheckProjs()
+	end
 end
